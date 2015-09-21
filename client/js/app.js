fd5a9f28d3c48a409a239a7179c86324a7908a18
jstodd <jstodd2612@gmail.com>
2015-09-21 12:27:32 -0600

    Finished AngularAssignment 1

--- client/js/app.js
+++ client/js/app.js
@@ -3,17 +3,58 @@ angular.module('RepsApp', [
 ]); // <--- Have to pass in an array[] to declare it as a new module.
 
 angular
-  .module('RepsAppControllers', [])
-  .controller(
-    'MainCtrl', function($http){
-      var main = this;
-      main.reps = [];
+  .module('RepsAppControllers', [
+    'repsService'
+  ])
+  .controller('MainCtrl', function (reps) {
+    var main = this;
+    main.reps = [];
 
-      main.searchByZip = function (zip){
-        $http
-          .get('http://dgm-representatives.herokuapp.com/all/by-zip/' + zip)
-          .then(function(res){
-            main.reps = res.data;
+    main.chooseType = 'reps';
+
+    function assignReps(data) {
+      main.reps = data;
+    }
+
+    main.apis = [
+      {
+        label: 'Name',
+        method: function (name) {
+          console.log("name working");
+          reps.searchByType(main.chooseType,'name', name).then(assignReps);
+        }
+      },
+      {
+        label: 'State',
+        method: function (state) {
+          console.log("State is Workinginsh");
+          reps.searchByType(main.chooseType,'state', state).then(assignReps);
+        }
+      },
+      {
+        label: 'Zip',
+        method: function (zip) {
+          console.log("Zip is Workingish");
+          reps.searchByType('all', 'zip', zip).then(assignReps);
+        }
+      }
+    ];
+
+    main.selectedApi = main.apis[0];
+
+  });
+
+angular
+  .module('repsService', [])
+  .factory('reps', function ($http) {
+    var host = 'http://dgm-representatives.herokuapp.com';
+    return {
+      searchByType: function(type, sort, name) {
+        return $http
+          .get(host + '/' + type + '/by-' + sort + '/' + name)
+          .then(function(response){
+            return response.data;
         });
-      };
+      }
+    }
   });
